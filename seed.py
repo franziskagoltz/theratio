4e5642d2ef1f2184be65906ecbd24cdab10472f4
Lauren Pritchett <laurenpritchett2@gmail.com>
2017-03-18 22:10:10 +0000

    modified seed.py

--- seed.py
+++ seed.py
@@ -19,48 +19,26 @@ def load_companies():
     db.session.commit()
 
 
-# def load_numbers():
-#     """Load numbers from numbers.txt into database."""
+def load_numbers():
+    """Load numbers from numbers.txt into database."""
 
-#     print "Numbers"
+    print "Numbers"
 
-#     Number.query.delete()
+    Number.query.delete()
 
-#     for row in open("seed_data/companies.txt"):
-#         row = row.rstrip()
-#         percent_women, percent_womentech, percent_womenleaders, nps_score = row.split("|")
-#         date = datetime.now()
-#         number = Number(percent_women=percent_women,
-#                         percent_womentech=percent_womentech,
-#                         percent_womenleaders=percent_womenleaders,
-#                         date=date,
-#                         nps_score=nps_score)
-#         db.session.add(number)
+    for row in open("seed_data/companies.txt"):
+        row = row.rstrip()
+        company_id, percent_women, percent_womentech, percent_womenleaders, nps_score = row.split("|")
+        date = datetime.now()
+        number = Number(company_id=company_id,
+                        percent_women=percent_women,
+                        percent_womentech=percent_womentech,
+                        percent_womenleaders=percent_womenleaders,
+                        date=date,
+                        nps_score=nps_score)
+        db.session.add(number)
 
-#     db.session.commit()
-
-
-
-# def load_users():
-#     """Load users from users_data into database."""
-
-#     print "Users"
-
-#     User.query.delete()
-
-#     for row in open("seed_data/users.txt"):
-#         row = row.rstrip()
-#         user_id, first_name, last_name, email, password = row.split("|")
-
-#         user = User(user_id=user_id,
-#                     first_name=first_name,
-#                     last_name=last_name,
-#                     email=email,
-#                     password=password)
-
-#         db.session.add(user)
-
-#     db.session.commit()
+    db.session.commit()
 
 
 if __name__ == '__main__':
