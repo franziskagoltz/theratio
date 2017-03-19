33dced565ae4bf23d40402c92f0e74b690f782cb
Jessica Appelbaum <jess.appelbaum@gmail.com>
2017-03-19 00:37:32 +0000

    More chart stuff

--- server.py
+++ server.py
@@ -41,42 +41,35 @@ def display_apple():
 def apple_info_data():
     """Chart information for Apple"""
 
+    data_dict = {
+                "labels": ["women overall"],
+                "datasets": [
+                {
+                    "data": [32],
+                    "backgroundColor": ["#ec008c"],
+                    "hoverBackgroundColor": ["#ec008c"]
+                }]}
+
     # data_dict = {
-    #             "labels": ["\% of women employees overall"],
-    #             "datasets": [
-    #             {
-    #                 "data": [32],
-    #                 "backgroundColor": ["#ec008c"]
-    #             }]}
+    #     "labels": [
+    #     "Christmas Melon",
+    #     "Crenshaw",],
+    #     "datasets": [
+    #                 {
+    #                 "data": [300, 50],
+    #                 "backgroundColor": [
+    #                 "#FF6384",
+    #                 "#36A2EB",
+    #                     ],
+    #                     "hoverBackgroundColor": [
+    #                         "#FF6384",
+    #                         "#36A2EB",
+    #                     ]
+    #                 }]
+    #         }
 
-    data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-        {
-            label: "My First dataset",
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1,
-            data: [65, 59, 80, 81, 56, 55, 40],
-        }
-        ]
-        }
 
-    return jsonify(data)
+    return jsonify(data_dict)
 
 
 @app.route("/company/google")
