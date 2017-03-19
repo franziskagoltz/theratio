5f34c40e935b23a298250c4365d73d5419d0a847
Jessica Appelbaum <jess.appelbaum@gmail.com>
2017-03-19 00:13:49 +0000

    chart code

--- server.py
+++ server.py
@@ -37,18 +37,46 @@ def display_apple():
 
     return render_template("company_info.html", name=name)
 
-@app.route('/apple-info.json')
+@app.route("/apple-info.json")
 def apple_info_data():
     """Chart information for Apple"""
 
-    data_dict = {
-                "labels": [
-                        "\% of women employees overall"],
-                "data:" [32],
-                "backgroundColor": ["#ffffff"]
-    }
+    # data_dict = {
+    #             "labels": ["\% of women employees overall"],
+    #             "datasets": [
+    #             {
+    #                 "data": [32],
+    #                 "backgroundColor": ["#ec008c"]
+    #             }]}
 
-    return jsonify(data_dict)
+    data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1,
+            data: [65, 59, 80, 81, 56, 55, 40],
+        }
+        ]
+        }
+
+    return jsonify(data)
 
 
 @app.route("/company/google")
--- static/js/apple.js
+++ static/js/apple.js
@@ -3,10 +3,10 @@ var options = { responsive: true };
 var ctx_donut = $("#barChart").get(0).getContext("2d");
 
 $.get("/apple-info.json", function (data) {
-    var myBarChart = new Chart(ctx_donut, {
-                                            type: 'horizontalBar',
-                                            data: data,
-                                            options: options
+    var myBarChart = new Chart(ctx, {
+                                    type: 'horizontalBar',
+                                    data: data,
+                                    options: options
                                           });
     $('#barLegend').html(myBarChart.generateLegend());
 });
